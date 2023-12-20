ede4d9aa4936c709e15644428a94b39ba2096743
Rustem <rustem.reizov.developer@gmail.com>
2023-12-20 17:39:41 +0300

    create RestaurantServiceClient

---
+++ swiggy-app/src/main/java/com/javatechie/swiggyapp/client/RestaurantServiceClient.java
@@ -0,0 +1,19 @@
+package com.javatechie.swiggyapp.client;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import com.javatechie.swiggyapp.dto.OrderResponseDTO;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class RestaurantServiceClient {
+	
+	private RestTemplate template;
+	
+	public OrderResponseDTO fetchOrderStatus(String orderId) {
+		return template.getForObject("http://RESTAURANT-SERVICE/restaurant/order/status/" + orderId, OrderResponseDTO.class);
+	}
+}
